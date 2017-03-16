025a8cc0e3ec5cd715db7ef616cc7873754c86fa
swimly <979741120@qq.com>
2017-03-16 18:29:42 +0800

    美化404页面

--- src/views/notfound.vue
+++ src/views/notfound.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    404
+  <div class="w h row bg-f7 notfound">
+    <div class="col v-m t-c">
+      <img src="static/img/404.gif" alt="">
+      <p>您访问的页面正在睡觉，请择时再来！</p>
+    </div>
   </div>
 </template>
 <script>
@@ -13,3 +16,6 @@ export default {
   }
 }
 </script>
+<style>
+.notfound p{font-size:16px;color:#999;}
+</style>
