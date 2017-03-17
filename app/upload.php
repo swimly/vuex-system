028a2cae23a435e844d0bc622c7cb56aa9fce476
swimly <979741120@qq.com>
2017-03-17 18:39:55 +0800

    删除php处理逻辑

--- app/upload.php
+++ app/upload.php
@@ -1,13 +1,14 @@
 <?
   header('Access-Control-Allow-Origin:*');
-  $base64_image_content=$_GET['url'];
-  $base64_image_content=str_replace('data:image/png;base64,', '', $base64_image_content);
-  $path='upload/';
-  $prefix ='face';
-  $output_file = $prefix.time().rand(100,999).'.jpg';
-  $path = $path.$output_file;
-  $ifp = fopen( $path, "wb" );
-  fwrite( $ifp, base64_decode( $base64_image_content) );
-  fclose( $ifp );
-  echo $path;
+  // $base64_image_content=$_GET['url'];
+  // $base64_image_content=str_replace('data:image/png;base64,', '', $base64_image_content);
+  // $path='upload/';
+  // $prefix ='face';
+  // $output_file = $prefix.time().rand(100,999).'.jpg';
+  // $path = $path.$output_file;
+  // $ifp = fopen( $path, "wb" );
+  // fwrite( $ifp, base64_decode( $base64_image_content) );
+  // fclose( $ifp );
+  // echo $path;
+  echo 1;
 ?>
